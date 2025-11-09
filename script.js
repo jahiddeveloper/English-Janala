618d229c85bf68935a6c49f582e541e033c5b487
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-11-09 08:12:36 +0600

    halka edit

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ let loadLesson = () => {
         .then(data => displayLesson(data.data))
 }
 
+let displayLesson = (lessons) => {
+
+    let lessonsContainer = document.getElementById("lesson-container");
+    lessonsContainer.innerHTML = "";
+
+    for (let lesson of lessons) {
+
+        let div = document.createElement("div")
+
+        div.innerHTML = `
+    
+    <button onclick="loadLessonWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+
+    `
+        lessonsContainer.append(div)
+    }
+}
+loadLesson();
+
+// Load Cart
+
 let loadLessonWord = (id) => {
     let url = `https://openapi.programming-hero.com/api/level/${id}`
 
@@ -44,21 +65,3 @@ let displayLoadLessonWord = (lessonWords) => {
     }
 }
 
-let displayLesson = (lessons) => {
-
-    let lessonsContainer = document.getElementById("lesson-container");
-    lessonsContainer.innerHTML = "";
-
-    for (let lesson of lessons) {
-
-        let div = document.createElement("div")
-
-        div.innerHTML = `
-    
-    <button onclick="loadLessonWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
-
-    `
-        lessonsContainer.append(div)
-    }
-}
-loadLesson();
